393156074708c4a9cd22121757f62435be2ae938
BrianLevin <bml201095@gmail.com>
2021-06-01 18:32:01 -0400

    deleted the  hello componend  and other files and added a card component

---
+++ src/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Card = () => {
+
+    return   (
+        
+        <div>
+<img src= ""  alt ="robots" />
+
+<div>
+<h1>Jane Doe</h1>
+
+<p>jane.doe@gmail.com</p>
+
+</div>
+
+
+
+        </div>
+
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './Hello';
-
+import Card from './Card';
 import 'tachyons';
 
-ReactDOM.render(  <Hello greeting=  {'Hello' + 'React Ninja'} />,document.getElementById('root'));
+ReactDOM.render(  <Card />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
