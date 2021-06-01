2f99f7f8c9297de4d4dfd31785fc549fc85458c0
BrianLevin <bml201095@gmail.com>
2021-06-01 01:03:35 -0400

    Added a css file and installed tacyons to format the html which utilizes Css making it fast loading, highly readable, and little custom CSS as possible

--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import './Hello.css'
+
 class Hello extends Component {
 
     render() {
 
 
          return (
-         <div>
+         <div class = 'f1 tc'>
          
          <h1>Hello World!</h1>
          <p>Welcome To React</p>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './Hello';
 
+import 'tachyons';
+
 ReactDOM.render(  <Hello />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
