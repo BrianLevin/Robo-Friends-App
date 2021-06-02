a2517e7a5d90711eb4e6cddaa4655ad1d3f94312
BrianLevin <bml201095@gmail.com>
2021-06-02 00:32:02 -0400

    made a cardlist compoenent which will list all the robots

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
 import 'tachyons';
+import CardList from './CardList'
 
 import { robots } from './robots';
 
 ReactDOM.render( 
-    <div>
-    <Card id=  {robots[0].id} name= {robots[0].name} email= {robots[0].email} />
-    <Card  id=  {robots[1].id} name= {robots[1].name} email= {robots[1].email} />
-    <Card  id=  {robots[2].id} name= {robots[2].name} email= {robots[2].email} />
-    </div>
-    
-    ,document.getElementById('root'));
+ 
+    <CardList robots = {robots} />
+    , document.getElementById('root'));
     
 
 // If you want to start measuring performance in your app, pass a function
