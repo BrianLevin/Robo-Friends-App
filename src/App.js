d59f19f1a8ee332cf12da8a6e1ced8fe3a81c338
BrianLevin <bml201095@gmail.com>
2021-06-03 00:31:10 -0400

    made an App componeent and export it to the app.js file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Cardlist from "./Cardlist";
+
+import { robots } from './robots';
+
+const App = () => {
+
+    return(
+
+        <CardList robots = {robots} />
+
+
+
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import CardList from './CardList'
+import App from './App';
 
-import { robots } from './robots';
 
-ReactDOM.render( 
- 
-    <CardList robots = {robots} />
-    , document.getElementById('root'));
+
+ReactDOM.render( <App />,  document.getElementById('root'));
     
 
 // If you want to start measuring performance in your app, pass a function
