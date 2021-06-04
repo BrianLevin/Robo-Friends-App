565c329ee9c36663e5637119d1425cfe02850598
BrianLevin <bml201095@gmail.com>
2021-06-04 15:47:41 -0400

    Added an onchange function in the input box

--- src/App.js
+++ src/App.js
@@ -5,26 +5,34 @@ import SearchBox from "./SearchBox";
 import { robots } from './robots';
 
 
-const state = {
 
-    robots: robots,
-    searchfield: ''
-}
 
 
 class App extends Component {
     constructor(){
+        super()
 this.state = {
-    
+    robots: robots,
+    searchfield: ''
 }
 
     }
+
+    onSearchChange(event) {
+
+console.log(event)
+
+
+    }
+
+
+
     render(){
         return(
             <div className = "tc">
     <h1> RoboFriends</h1>
-    <SearchBox />
-            <CardList robots = {robots} />
+    <SearchBox  searchChange=  {this. onSearchChange}/>
+            <CardList robots = {this.state.robots} />
     
             </div>
     
