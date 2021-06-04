c996d3d06e85ff7a900013075045c2961f2ba6b9
BrianLevin <bml201095@gmail.com>
2021-06-04 16:29:44 -0400

    sucessfully completed the filtered robots

--- src/App.js
+++ src/App.js
@@ -22,21 +22,20 @@ this.state = {
     
 this.setState({searchfield: event.target.value})
 
-const filteredRobots = this.state.robots.filter(robots => {
-    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase)
-})
-console.log(filteredRobots);
-
     }
 
 
 
     render(){
-        return(
+        
+            const filteredRobots = this.state.robots.filter(robots => {
+                return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            })
+            return(
             <div className = "tc">
     <h1> RoboFriends</h1>
-    <SearchBox  searchChange=  {this. onSearchChange}/>
-            <CardList robots = {this.state.robots} />
+    <SearchBox  searchChange=  {this.onSearchChange}/>
+            <CardList robots = {filteredRobots} />
     
             </div>
     
