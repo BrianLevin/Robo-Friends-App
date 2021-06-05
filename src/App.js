fb28424b11ab9ca97701a6bfc0408abfd030fe12
BrianLevin <bml201095@gmail.com>
2021-06-04 23:57:41 -0400

    fetched the data from the users api to display the robots

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
 import "./App.css";
 
 
@@ -13,10 +12,17 @@ class App extends Component {
     constructor(){
         super()
 this.state = {
-    robots: robots,
+    robots: [],
     searchfield: ''
 }
 
+    }
+
+    componentDidMount() {
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response =>response.json())
+.then(users =>this.setState({robots: users}));
+       
     }
 
     onSearchChange= (event) => {
@@ -34,7 +40,7 @@ this.setState({searchfield: event.target.value})
             })
             return(
             <div className = "tc">
-    <h1> RoboFriends</h1>
+    <h1 className= "f1"> RoboFriends</h1>
     <SearchBox  searchChange=  {this.onSearchChange}/>
             <CardList robots = {filteredRobots} />
     
